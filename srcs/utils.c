6fe34a2635063bfd86bfdbdbfd68a14ce43a70f1
Steven Lippert <slippert@2-D-4.42heilbronn.de>
2023-12-22 15:04:20 +0100

    auto commit

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: slippert <slippert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 21:08:51 by jsanger           #+#    #+#             */
-/*   Updated: 2023/12/22 14:48:55 by slippert         ###   ########.fr       */
+/*   Updated: 2023/12/22 15:04:17 by slippert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,13 @@ void	reset(t_data *data)
 void ft_free2d(char **str)
 {
 	for (int i = 0; str[i] != NULL; i++)
-	{
 		free(str[i]);
-	}
 	free(str);
 }
 
 void	ft_exit(t_data *data)
 {
-	// ft_free2d(data->map->map);
+	ft_free2d(data->map->map);
 	mlx_terminate(data->mlx);
 	free(data->map);
 	free(data);
