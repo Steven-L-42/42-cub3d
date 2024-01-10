04865a4538e691d7b68b202fa143ca7ce048f4d9
Steven Lippert <s.lippertdev@gmail.com>
2024-01-10 23:59:57 +0100

    test

--- SegTester/SegTester.c
+++ SegTester/SegTester.c
@@ -37,3 +37,28 @@ int	main(void)
 		re_open();
 	return (0);
 }
+
+
+// void	re_open(void)
+// {
+// 	const char 	*val_param = "valgrind --error-exitcode=1 --error-limit=no";
+// 	const char	*prog_path = "./cub3d";
+// 	const char	*prog_param = "maps/map.cub";
+// 	char		command[256];
+
+// 	snprintf(command, sizeof(command), "%s %s %s",val_param, prog_path, prog_param);
+// 	system(command);
+// }
+
+// int	main(void)
+// {
+// 	const char	*dir_path = "/home/oem/Desktop/42-cub3d/";
+
+// 	if (chdir(dir_path) != 0)
+// 	{
+// 		perror("Fehler beim Wechseln des Verzeichnisses");
+// 	}
+// 	while (1)
+// 		re_open();
+// 	return (0);
+// }
